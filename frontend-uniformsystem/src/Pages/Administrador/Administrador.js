fcc95010bd211e240f4cb95b51e5de6a687780e1
Gabriel Andrade <gabrielolandrade@hotmail.com.br>
2020-11-02 12:00:06 -0300

    Adicionada todas as páginas com estruturas básicas

---
+++ frontend-uniformsystem/src/Pages/Administrador/Administrador.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+function Adminstrador(){
+  return (
+    <div>
+      Pagina Adminstrador
+    </div>
+  );
+}
+
+export default Adminstrador;
--- frontend-uniformsystem/src/routes.js
+++ frontend-uniformsystem/src/routes.js
@@ -2,12 +2,22 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./Pages/Home";
+import Administrador from "./Pages/Administrador";
+import Loja from "./Pages/Loja";
+import Perfil from "./Pages/Perfil";
+import Carrinho from "./Pages/Carrinho";
+import Contato from "./Pages/Contato";
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/home" export exact component={Home} />
+                <Route path="/adm" export exact component={Administrador} />
+                <Route path="/shop" export exact component={Loja} />
+                <Route path="/perfil" export exact component={Perfil} />
+                <Route path="/car" export exact component={Carrinho} />
+                <Route path="/contact" export exact component={Contato} />
             </Switch>
         </BrowserRouter>
     );

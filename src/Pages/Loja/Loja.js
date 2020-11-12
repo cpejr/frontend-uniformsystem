67720178fcf9e92e96247c5624f9ea53500f1f08
Maria <mariaclaracaneschi15@gmail.com>
2020-11-12 19:11:03 -0300

    Iniciando a funcionalidade do filtro

--- src/Pages/Loja/Loja.js
+++ src/Pages/Loja/Loja.js
@@ -25,16 +25,17 @@ const PRICE_OPTIONS = [
 function Loja() {
 
   const [products, setProducts] = useState([]);
+  const [route, setRoute] = useState('/productmodels');
 
   async function getProducts() { //fazendo a requisição pro back
     try {
-      const response = await api.get("/productmodels");
+      const response = await api.get(`${route}`);
       console.log(response);
       setProducts([...response.data.models]);
     }
     catch (error) {
       console.warn(error)
-      alert("Erro no servidor");
+      alert("Erro no servidor.");
     }
   }
 
@@ -42,45 +43,74 @@ function Loja() {
     getProducts()
   }, [])
 
+
+  const [filter, setFilter] = useState([]);
+  const [aux, setAux] = useState(route);
+
+  function handleInputChange(e) {
+    const id = e.target.id;
+    const query = ['gender=M', 'gender=F', 'product_type=sport'];
+    if (filter.indexOf(id) === -1) {
+      const newFilter = [...filter, id];
+      setFilter(newFilter);
+      if (id === 'filter-1') {
+        const teste = [aux, query[1]];
+        setAux(teste.join('?'));
+      }
+      if (id === 'filter-2') {
+        const teste = [aux, query[0]];
+        setAux(teste.join('&'));
+      }
+      if (id === 'filter-3') {
+        const teste = [aux, query[2]];
+        setAux(teste.join('&'));
+      }
+    }
+  }
+
+
   return (
-    <div className="shopContainer">
+    <div className="shop">
+      <div className="shopContainer">
+        <p>{filter.join(" ")}</p>
 
-      <div className="filterContainer">
+        <div className="filterContainer">
 
-        <div className="filterTitleProducts">
-          <FaFilter />  FILTROS
+          <div className="filterTitleProducts">
+            <FaFilter />  FILTROS
         </div>
 
-        {FILTER_OPTIONS.map((option, index) => {
-          return (
-            <div className="filtersProducts">
-              <input type="checkbox" id={`filter-${index}`} name="filter" />
-              <label for={`filter-${index}`}>{option}</label>
-            </div>
-          )
-        })}
-
-        <div className="priceContainer">
-          <p>PREÇO</p>
-
-          {PRICE_OPTIONS.map((price, index) => {
+          {FILTER_OPTIONS.map((option, index) => {
             return (
-              <div className="filterPrice">
-                <label for={`price-${index}`}>{price}</label>
-                <input type="radio" id={`price-${index}`} name="price" />
+              <div className="filtersProducts">
+                <input type="checkbox" id={`filter-${index}`} name="filter" onChange={handleInputChange} />
+                <label for={`filter-${index}`}>{option}</label>
               </div>
             )
           })}
+
+          <div className="priceContainer">
+            <p>PREÇO</p>
+
+            {PRICE_OPTIONS.map((price, index) => {
+              return (
+                <div className="filterPrice">
+                  <label for={`price-${index}`}>{price}</label>
+                  <input type="radio" id={`price-${index}`} name="price" onChange={handleInputChange} />
+                </div>
+              )
+            })}
+          </div>
+
+        </div>
+
+        <div className="productContainer">
+          {products.map(product =>
+            <ProductCard key={product.product_model_id} product={product} />
+          )}
         </div>
 
       </div>
-
-      <div className="productContainer">
-        {products.map(product =>
-          <ProductCard key={product.product_model_id} product={product} />
-        )}
-      </div>
-
     </div>
   );
 }

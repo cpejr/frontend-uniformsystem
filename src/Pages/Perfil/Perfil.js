8a6f0e3d3b7135594b6822a4ea66c721d0704d05
Gabriel <gabrieljunqueirasouza@hotmail.com>
2020-11-18 15:03:52 -0300

    containers pedidos quase prontos

--- src/Pages/Perfil/Perfil.js
+++ src/Pages/Perfil/Perfil.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import CardPedido from "../../components/CardPedido"
 import './Perfil.css'
+
+const pedidos = [
+  {
+    id: 1,
+    pedidoNumber:"Pedido: #12345",
+    data:"01/01/2020",
+    status:"Status: Em andamento",
+    destino:"Destino: Não enviado",
+    total:"Total: R$ 100,00",
+    button:"Acompanhar o pedido",
+  },
+  {
+    id: 2,
+    pedidoNumber:"Pedido: #12346",
+    data:"01/01/2020",
+    status:"Status: Finalizado",
+    destino:"Destino: Confirmado",
+    total:"Total: R$ 150,00",
+    button:"Acompanhar a entrega",
+  },
+]
 
 function Perfil(){
   return (
-    <div>
-      Pagina Perfil
+    <div className="containerPedidos">
+      {pedidos.map((pedido) => (
+        <CardPedido key={pedido.id} pedido={pedido} />
+      ))}
     </div>
   );
 }

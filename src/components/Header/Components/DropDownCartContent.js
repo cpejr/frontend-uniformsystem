b8b06cff69a4b990565b04ed221c10a4e4139682
Gabriel Andrade <gabrielolandrade@hotmail.com.br>
2020-11-18 10:06:46 -0300

    update: transcrevi o css inline para outro arquivo

--- src/components/Header/Components/DropDownCartContent.js
+++ src/components/Header/Components/DropDownCartContent.js
@@ -2,6 +2,8 @@ import React from "react";
 import { ClickAwayListener } from "@material-ui/core";
 import { FaShoppingCart } from "react-icons/fa";
 
+import "./style.css";
+
 const Products_in_Cart = [
     {
         name: "Camisa 1",
@@ -53,113 +55,30 @@ export default function DropDownCartContent(props) {
 
             return (
                 <>
-                    <div
-                        className="singleProduct"
-                        style={{
-                            boxSizing: "border-box",
-                            width: "95%",
-                            height: "90px",
-                            margin: "auto",
-                            padding: "0px 1px 0px 3px",
-                            color: "black",
-
-                            display: "flex",
-                            justifyContent: "space-between",
-                        }}
-                    >
+                    <div className="singleProduct">
                         <div
                             className="photo"
                             /* Provavel que seja necessario um width ali. Nao coloquei por enquanto */
-                            style={{
-                                height: "90%",
-                                maxWidth: "33%",
-                                margin: "auto",
-                            }}
                         >
                             {/* Na linha abaixo deveria entrar uma variavel do objeto, no campo SRC. */}
                             <img
                                 src={produto.imgLink}
                                 alt="FotoCamisa"
-                                style={{
-                                    width: "90%",
-                                    height: "90%",
-                                }}
+                                className="FotoCamisa"
                             />
                         </div>
-                        <div
-                            className="texts"
-                            style={{
-                                width: "67%",
-                                display: "flex",
-                                flexDirection: "column",
-                                height: "100%",
-                            }}
-                        >
-                            <h4
-                                style={{
-                                    margin: "10px 0px",
-                                    fontWeight: "lighter",
-                                }}
-                            >
-                                {produto.name}
-                            </h4>
-                            <div
-                                className="description"
-                                syle={{ color: "black" }}
-                            >
-                                <div
-                                    className="pt1"
-                                    style={{
-                                        display: "flex",
-                                        justifyContent: "space-between",
-                                        width: "100%",
-                                        fontSize: "12px",
-                                        color: "#666",
-                                        margin: "3px 0",
-                                    }}
-                                >
-                                    <p
-                                        style={{
-                                            width: "fit-content",
-                                            margin: "0",
-                                        }}
-                                    >
+                        <div className="texts">
+                            <h4 className="Prod_Title">{produto.name}</h4>
+                            <div className="description">
+                                <div className="pt1">
+                                    <p className="size">
                                         Tamanho: {produto.size}
                                     </p>
-                                    <p
-                                        style={{
-                                            width: "fit-content",
-                                            margin: "0",
-                                        }}
-                                    >
-                                        R$ {produto.price}
-                                    </p>
+                                    <p>R$ {produto.price}</p>
                                 </div>
-                                <div
-                                    className="pt2"
-                                    style={{
-                                        display: "flex",
-                                        justifyContent: "space-between",
-                                        width: "100%",
-                                        fontSize: "12px",
-                                        color: "#666",
-                                    }}
-                                >
-                                    <p
-                                        style={{
-                                            width: "fit-content",
-                                            margin: "0",
-                                        }}
-                                    >
-                                        Cor: Branca
-                                    </p>
-                                    <p
-                                        style={{
-                                            width: "fit-content",
-                                            margin: "0",
-                                            fontSize: "12px",
-                                        }}
-                                    >
+                                <div className="pt2">
+                                    <p className="color">Cor: Branca</p>
+                                    <p className="total_price">
                                         {produto.quantity}xR$ {produto.price}
                                     </p>
                                 </div>
@@ -183,61 +102,12 @@ export default function DropDownCartContent(props) {
 
     return (
         <ClickAwayListener onClickAway={handleClickAway}>
-            <div
-                className="all_window"
-                style={{
-                    position: "absolute",
-                    top: "25px",
-                    right: "20px",
-                    display: "flex",
-                    flexDirection: "column",
-                    backgroundColor: "#fff",
-                    height: "fit-content",
-                    width: "350px",
-                    boxSizing: "content-box",
+            <div className="all_window">
+                <div className="blueLine"></div>
 
-                    borderLeft: "solid 0.5px rgba(0,0,0,0.2)",
-                    borderRight: "solid 0.5px rgba(0,0,0,0.2)",
-                    borderBottom: "solid 0.5px rgba(0,0,0,0.2)",
-                    /* paddingTop:'10px' */
-                }}
-            >
-                <div
-                    className="blueLine"
-                    style={{
-                        width: "100.60%",
-                        height: "4px",
-                        backgroundColor: "#4DCDBC",
-                        marginBottom: "15px",
-                        margin: "-1.5px",
-                        zIndex: "9",
-                    }}
-                ></div>
+                <div className="products">{SingleProducts()}</div>
 
-                <div
-                    className="products"
-                    style={{
-                        minHeight: "90px",
-                        maxHeight: "175px",
-                        overflow: "auto",
-                        width: "100%",
-                        margin: "auto",
-                        padding: "10px 0px",
-                    }}
-                >
-                    {SingleProducts()}
-                </div>
-
-                <div
-                    className="subtotal_div"
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        width: "90%",
-                        color: "black",
-                        margin: "auto",
-                    }}
-                >
+                <div className="subtotal_div">
                     <h5 className="subtotal" style={{ color: "black" }}>
                         SUBTOTAL{" "}
                     </h5>
@@ -245,20 +115,7 @@ export default function DropDownCartContent(props) {
                         R$ {Subtotal},00
                     </h5>
                 </div>
-                <button
-                    style={{
-                        margin: "auto",
-                        width: "fit-content",
-                        color: "black",
-                        marginBottom: "10px",
-                        backgroundColor: "#4DCDBC",
-                        padding: "5px 20px",
-                        borderRadius: "15px",
-                        border: "0",
-                        display: "flex",
-                        alignItems: "center",
-                    }}
-                >
+                <button className="stylized_button">
                     Ir para o carrinho {"  "}
                     <FaShoppingCart className="cart_icon" style={{}} />
                 </button>
--- src/components/Header/Components/DropDownLoginContent.js
+++ src/components/Header/Components/DropDownLoginContent.js
@@ -14,25 +14,27 @@ export default function DropDownLoginContent(props) {
     async function handleLogin(e) {
         e.preventDefault();
 
-        handleClickAway()
+        handleClickAway();
 
         try {
-            const response = await api.post("login", { email:User, password:Password });
+            const response = await api.post("login", {
+                email: User,
+                password: Password,
+            });
 
-            
             console.log(response, "o response");
-            
+
             localStorage.setItem("accessToken", response.data.accessToken);
-            
+
             const user = response.data.user[0];
-            
+
             console.log(user);
-            alert('Bem vindo, '+ user.name)
+            alert("Bem vindo, " + user.name);
         } catch (error) {
             /* setError(error.response.data.message); */
             console.error(error);
 
-            alert(error.response.data.message)
+            alert(error.response.data.message);
         }
 
         console.log(User, "<- User|| Password -> ", Password);
@@ -41,57 +43,12 @@ export default function DropDownLoginContent(props) {
 
     return (
         <ClickAwayListener onClickAway={handleClickAway}>
-            <form
-                className="drop_content"
-                style={{
-                    border: "solid 0.5px #ccc",
-                    borderRadius: "10px",
-                    width: "300px",
-                    height: "200px",
-                    backgroundColor: "#fff",
-                    position: "absolute",
-                    top: "23px",
-                    right: "0px",
-                }}
-            >
-                <div
-                    className="title_login"
-                    style={{
-                        width: "100%",
-                        backgroundColor: "#ccc",
-                        height: "fit-content",
-                        textAlign: "center",
-                        borderRadius: "10px 10px 0 0",
-                        padding: "10px 0",
-                        color: "black",
-                    }}
-                >
-                    LOGIN
-                </div>
-                <div
-                    className="inputs"
-                    style={{
-                        width: "80%",
-                        color: "black",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        margin: "auto",
-                        padding: "10px 0",
-                    }}
-                >
+            <form className="drop_content">
+                <div className="title_login">LOGIN</div>
+                <div className="inputs">
                     USUÁRIO
                     <input
                         className="input_login"
-                        style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            color: "black",
-                            width: "95%",
-                            borderRadius: "15px",
-                            border: "solid 1px #ccc",
-                        }}
                         type="text"
                         onChange={(e) => {
                             setUser(e.target.value);
@@ -99,14 +56,6 @@ export default function DropDownLoginContent(props) {
                     />
                     SENHA
                     <input
-                        style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            color: "black",
-                            width: "95%",
-                            borderRadius: "15px",
-                            border: "solid 1px #ccc",
-                        }}
                         className="input_password"
                         type="password"
                         onChange={(e) => {
@@ -114,59 +63,14 @@ export default function DropDownLoginContent(props) {
                         }}
                     />
                 </div>
-                <div
-                    className="buttons"
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        width: "80%",
-                        margin: "auto",
-                    }}
-                >
-                    <button
-                        className="b_login"
-                        onClick={(e) => handleLogin(e)}
-                        style={{
-                            margin: "auto",
-                            width: "fit-content",
-                            color: "black",
-                            marginBottom: "10px",
-                            backgroundColor: "#4DCDBC",
-                            padding: "5px 20px",
-                            borderRadius: "15px",
-                            border: "0",
-                            display: "flex",
-                            alignItems: "center",
-                        }}
-                    >
+                <div className="buttons">
+                    <button className="b_login" onClick={(e) => handleLogin(e)}>
                         ACESSAR
                     </button>
 
-                    <button
-                        className="b_register"
-                        style={{
-                            margin: "auto",
-                            width: "fit-content",
-                            color: "black",
-                            marginBottom: "10px",
-                            backgroundColor: "#4DCDBC",
-                            padding: "5px 20px",
-                            borderRadius: "15px",
-                            border: "0",
-                            display: "flex",
-                            alignItems: "center",
-                        }}
-                    >
-                        CADASTRAR
-                    </button>
+                    <button className="b_register">CADASTRAR</button>
                 </div>
-                <a
-                    href={"/forgetPassword"}
-                    style={{
-                        margin: "0 10px",
-                        color: "#666",
-                    }}
-                >
+                <a className="forgetPassword" href={"/forgetPassword"}>
                     Esqueceu sua senha?
                 </a>
             </form>

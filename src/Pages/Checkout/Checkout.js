9205efa83466604d9dfdc78cdae2a48e83a34b0c
Diogo Almazan <diogodpa@outlook.com>
2020-11-19 21:57:24 -0300

    Checkout structure page done

---
+++ src/Pages/Checkout/Checkout.js
@@ -0,0 +1,102 @@
+import React from 'react';
+
+import camisa from '../../Assets/camisa.jpg';
+
+
+import './Checkout.css'
+
+
+function Checkout(){
+
+  return (
+    <div className="fullContent">
+
+      <h1>Lista de Produtos</h1>
+      <div className="mainContent">
+        <div className="leftSide">
+
+          <div className="aboutProduct">
+            <img src={camisa} alt=""/>
+            <div className="infoProduct">
+              <span>
+                Nome do produto: Produto
+              </span>
+              <span>
+                Quantidade total: 10 uni.
+              </span>
+              <span>
+                Cor: Branco
+              </span>
+              <span>
+                Preço: R$ 500,00
+              </span>
+            </div>
+          </div>
+            <div className="aboutPayment">
+              <h2>Pagamento</h2>
+
+              Acordeão
+            </div>
+        </div>
+
+        <div className="rightSide">
+          <div className="productSummary">
+            <div className="summaryTitle">
+              <strong>Resumo do Pedido</strong>
+            </div>
+
+            <div className="addressInfo">
+
+              <div className="addressConfirmation">
+                <strong>Endereço de entrega</strong>
+                <span>
+                  Rua Fulano de Tal, 900
+                </span>
+                <span>
+                  Bairro: Esse aqui
+                </span>
+                <span>
+                  Cidade: Belo Horizonte - MG
+                </span>
+                <span>
+                  CEP: 00000-000
+                </span>
+              </div>
+
+              <div className="changeAddressArea">
+                <span>Quer receber seus produtos em outro endereço?</span>
+                <button type="button">Alterar endereço</button>
+              </div>
+            </div>
+
+            <div className="shippingInfo">
+              <strong>Frete</strong>
+              <div className="valueShipping">
+                <div className="leftValueShipping">
+                  <img src="" alt=""/>
+                  <span>Valor</span>
+                </div> 
+                <div className="rightValueShipping">
+                  <span>R$25,00</span>
+                </div> 
+              </div>
+            </div>
+
+            <div className="totalArea">
+              <strong>Total</strong>
+              <strong>R$525,00</strong>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+      <button type="button" className="purchaseFinished">
+        Finalizar Compra
+      </button>
+    </div>
+  );
+}
+
+export default Checkout;

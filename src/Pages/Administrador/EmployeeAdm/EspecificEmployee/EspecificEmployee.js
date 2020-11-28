ce23e231cff6022d0dd44fd4c6eb8c08becd24c4
Gabriel <gabrieljunqueirasouza@hotmail.com>
2020-11-27 21:14:39 -0300

    Update: Pagina func. especifico

--- src/Pages/Administrador/EmployeeAdm/EspecificEmployee/EspecificEmployee.js
+++ src/Pages/Administrador/EmployeeAdm/EspecificEmployee/EspecificEmployee.js
@@ -6,11 +6,11 @@ function EspecificEmployee() {
   return (
     <table>
       <tr>
-        <th>Month</th>
-        <th>Savings</th>
+        <th>DADOS PESSOAIS</th>
+        <th>HISTÓRICO DE PEDIDOS</th>
       </tr>
       <tr>
-        <td>January</td>
+        <td>NOME COMPLETO: MARIA SOUZA</td>
         <td>$100</td>
       </tr>
     </table>

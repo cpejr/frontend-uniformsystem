f8ec2ecc7b58ea132465b0b50a1d044e90bd88a5
Gabriel <gabrieljunqueirasouza@hotmail.com>
2020-11-28 10:49:32 -0300

    Uptdate: Funcionário Específico quase pronto

--- src/Pages/Administrador/EmployeeAdm/EspecificEmployee/EspecificEmployee.js
+++ src/Pages/Administrador/EmployeeAdm/EspecificEmployee/EspecificEmployee.js
@@ -1,20 +1,57 @@
 import React from "react";
+import TabelaFuncionarios from "../../../../components/TabelaFuncionario/TabelaFuncionario";
 
 import "./EspecificEmployee.css";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+
+const funcionarios = [
+  {
+    id: 1,
+    nome: "MARIA SOUZA",
+    cpf: "123456789-35",
+  },
+];
+
+const historicoPedidos = [
+  {
+    id: 3,
+    idpedido: "#0003",
+    data: "05/11/2020",
+  },
+  {
+    id: 2,
+    idpedido: "#0002",
+    data: "04/11/2020",
+  },
+  {
+    id: 1,
+    idpedido: "#0001",
+    data: "03/11/2020",
+  },
+];
 
 function EspecificEmployee() {
   return (
-    <table>
-      <tr>
-        <th>DADOS PESSOAIS</th>
-        <th>HISTÓRICO DE PEDIDOS</th>
-      </tr>
-      <tr>
-        <td>NOME COMPLETO: MARIA SOUZA</td>
-        <td>$100</td>
-      </tr>
-    </table>
+    <div>
+      <ArrowBackIosIcon className="setaVoltar"></ArrowBackIosIcon>
+      <div className="boxTitle">
+        <h1 className="titleFuncExp">FUNCIONÁRIO ESPECÍFICO</h1>
+        <hr className="titleLineExp"></hr>
+      </div>
+      <div className="tabela">
+        {funcionarios.map((funcionario) => (
+          <TabelaFuncionarios key={funcionario.id} funcionario={funcionario} />
+        ))}
+      </div>
+    </div>
   );
 }
 
 export default EspecificEmployee;
+
+/*todo: arrowback funcional
+        tamanho arrowback
+        cor da borda
+        .map id e data
+        tirar barra esquerda (Diogão)
+*/

53590d68a9bd830a2753ecf129498df5da1e7c6e
Gabriel <gabrieljunqueirasouza@hotmail.com>
2020-12-11 09:06:51 -0300

    Update: Pequenas mudanças cadastro Func

--- src/Pages/Administrador/EmployeeAdm/CadastroFunc/CadastroFunc.js
+++ src/Pages/Administrador/EmployeeAdm/CadastroFunc/CadastroFunc.js
@@ -48,7 +48,7 @@ function CadastroFunc() {
       ) {
         setErrorInputPassword("");
       } else {
-        setErrorInputPassword("Senha inválida");
+        setErrorInputPassword("Senha inválida (min. 6 dígitos)");
       }
     }
   }

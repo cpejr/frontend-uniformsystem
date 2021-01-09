fa491c5a86ded15041bb9bb4f27d30b519ec5403
Diogo Almazan <diogodpa@outlook.com>
2021-01-09 13:37:27 -0300

    Validação de login e material ui adicionado

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { Link, Route } from 'react-router-dom';
+import { Button, TextField, CircularProgress } from '@material-ui/core';
 import { LoginContext } from "../../contexts/LoginContext";
 import api from "../../services/api";
 import './Login.css';
@@ -13,6 +14,14 @@ function Login(){
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorEmailMessage, setErrorEmailMessage] = useState('');
+
+  const [errorPassword, setErrorPassword] = useState(false);
+  const [errorPasswordMessage, setErrorPasswordMessage] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
   const handleEmail = (e) => {
     setEmail(e.target.value);
   }
@@ -21,25 +30,85 @@ function Login(){
     setPassword(e.target.value);
   }
 
-	async function handleSubmit(e) {
-    e.preventDefault();
-    
+  const validateInputs = (type, value) => {
 
-    try {
-      const response = await api.post("login", {email, password});
-      if (response.data && response.data.accessToken) {
-        const token = response.data.accessToken;
-        const user = response.data.user;
-        signIn(token, user);
-				//Aqui manda para a rota logo apos o login
-        history.push("/adm/home");
-      } else {
-        alert(`Usuario ou senha incorretos!`);
-      }
-    } catch (error) {
-      alert(`Acesso negado!`);
-      console.warn(error);
+    let isValid;
+
+    switch (type) {
+      case 'email':
+        if(value === '' || !value.includes('@') || !value.includes('.com')){
+          isValid = false;
+        }else{
+          isValid = true;
+        }
+        
+        break;
+        case 'password':
+          if(value === ''){
+            isValid = false;
+          }else{
+            isValid = true;
+          }
+        break;
+    
+      default:
+        break;
     }
+    return isValid;
+    
+  }
+
+	async function handleSubmit() {
+    
+    const isEmailValid = validateInputs('email', email);
+    const isPasswordValid = validateInputs('password', password);
+
+    if( !isEmailValid || !isPasswordValid ){
+      if(!isEmailValid){
+        setErrorEmail(true);
+        setErrorEmailMessage('E-mail inválido');
+      }else{
+        setErrorEmail(false);
+        setErrorEmailMessage('');
+      }
+
+      if(!isPasswordValid){
+        setErrorPassword(true);
+        setErrorPasswordMessage('Campo inválido');
+      }else{
+        setErrorPassword(false);
+        setErrorPasswordMessage('');
+      }
+    }else{
+
+      setLoading(true);
+
+      setErrorEmail(false);
+      setErrorEmailMessage('');
+      setErrorPassword(false);
+      setErrorPasswordMessage('');
+
+      try {
+        const response = await api.post("login", {email, password});
+        if (response.data && response.data.accessToken) {
+          const token = response.data.accessToken;
+          const user = response.data.user;
+          signIn(token, user);
+          //Aqui manda para a rota logo apos o login
+          history.push("/adm/home");
+        } else {
+          alert(`Usuario ou senha incorretos!`);
+        }
+        setLoading(false);
+
+      } catch (error) {
+        setLoading(false);
+        alert(`Acesso negado!`);
+        console.warn(error);
+      }
+
+    }
+
   }
   
   return (
@@ -49,10 +118,34 @@ function Login(){
           <form>
             <h1>Login</h1>
             <h5>Usuário</h5>
-            <input type="text" placeholder="Usuário" onChange={(e) => handleEmail(e)}/>
+            <TextField
+              variant="outlined"
+              type="text" 
+              label="Usuário"
+              error={errorEmail}
+              helperText={errorEmailMessage}
+              onChange={(e) => handleEmail(e)}
+            />
             <h5>Senha</h5>
-            <input type="password" placeholder="Senha" onChange={(e) => handlePassword(e)}/>
-            <button className= "button" type="submit" onChange={(e) => handleSubmit(e)}>Entrar</button>
+            <TextField
+              variant="outlined"
+              type="password" 
+              label="Senha"
+              error={errorPassword}
+              helperText={errorPasswordMessage}
+              onChange={(e) => handlePassword(e)}
+            />
+            <Button 
+              className= "button" 
+              color="primary"
+              onClick={() => handleSubmit()}
+            >
+              {
+                loading ?
+                  <CircularProgress/> :
+                  "Entrar"
+              }
+            </Button>
             <Link to="/register">Esqueci minha senha</Link>
           </form>
         </section>

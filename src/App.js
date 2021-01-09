36c1be43497b2f3ea38f6622db8bcb898d7307c9
Diogo Almazan <diogodpa@outlook.com>
2021-01-09 13:15:56 -0300

    Arquivo context criado e em andamento no Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 import Routes from "./routes";
+import LoginContextProvider from './contexts/LoginContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-    return <Routes />;
+    return (
+        <LoginContextProvider>
+            <Routes />;
+        </LoginContextProvider>
+    );
 }
 
 export default App;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState, useContext } from "react";
+import { Link, Route } from 'react-router-dom';
+import { LoginContext } from "../../contexts/LoginContext";
+import api from "../../services/api";
 import './Login.css';
 
 
 function Login(){
+
+  const { signIn } = useContext(LoginContext);
+  const { history } = Route;
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  }
+
+	async function handleSubmit(e) {
+    e.preventDefault();
+    
+
+    try {
+      const response = await api.post("login", {email, password});
+      if (response.data && response.data.accessToken) {
+        const token = response.data.accessToken;
+        const user = response.data.user;
+        signIn(token, user);
+				//Aqui manda para a rota logo apos o login
+        history.push("/adm/home");
+      } else {
+        alert(`Usuario ou senha incorretos!`);
+      }
+    } catch (error) {
+      alert(`Acesso negado!`);
+      console.warn(error);
+    }
+  }
+  
   return (
     <div className="login">
       <div className="box">
@@ -10,11 +49,11 @@ function Login(){
           <form>
             <h1>Login</h1>
             <h5>Usuário</h5>
-            <input placeholder="Usuário"></input>
+            <input type="text" placeholder="Usuário" onChange={(e) => handleEmail(e)}/>
             <h5>Senha</h5>
-            <input placeholder="Senha"></input>
-            <button className= "button" type="submit">Entrar</button>
-            <a href="/register">Esqueci minha senha</a>
+            <input type="password" placeholder="Senha" onChange={(e) => handlePassword(e)}/>
+            <button className= "button" type="submit" onChange={(e) => handleSubmit(e)}>Entrar</button>
+            <Link to="/register">Esqueci minha senha</Link>
           </form>
         </section>
         </div>

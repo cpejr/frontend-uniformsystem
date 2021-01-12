c256a7b4a79df976078b937ff01627d07de45cc4
João Menezes <joaopm.m0@gmail.com>
2021-01-12 19:22:27 -0300

    Update: colocando divs e seus css's

--- src/Pages/Contato/Contato.js
+++ src/Pages/Contato/Contato.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Contato.css'
+import camisa from '../../Assets/camisa.jpg';
 
 function Contato(){
   return (
     <div className="divPagContato">
       <div className="fundoCinza">
-        <div className="fundoBranco">
+        <div className="divFundoBrancoContato">
           <div className="tituloContato">
-            Contato
+            <h1>Contato</h1>
           </div>
           <div className="divRedesSociais">
-            Redes sociais
+            <h3>INSTAGRAM</h3>
+            <h3>wpp</h3>
+            <h3>Facebook</h3>
           </div>
           <div className="divEmail">
             email

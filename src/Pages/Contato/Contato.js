8804e8324f87e5fdec276726db2d1a044873b488
Felipe Monteiro <felipemonteiro@cpejr.com.br>
2021-01-13 20:11:06 -0300

    ADD Nomes de cima

--- src/Pages/Contato/Contato.js
+++ src/Pages/Contato/Contato.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { FaFacebook, FaWhatsapp, FaInstagram, FaMapMarkerAlt } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import './Contato.css'
+import React from "react";
+import {
+  FaFacebook,
+  FaWhatsapp,
+  FaInstagram,
+  FaMapMarkerAlt,
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import "./Contato.css";
 
-function Contato(){
+function Contato() {
   return (
     <div className="divPagContato">
       <div className="fundoCinza">
@@ -13,22 +18,31 @@ function Contato(){
           </div>
           <div className="divRedesSociais">
             <a href="https://www.instagram.com" className="linksContato">
-              <FaInstagram className="iconsContato"/>
+              <FaInstagram className="iconsContato" />
             </a>
             <a href="https://www.whatsapp.com" className="linksContato">
-              <FaWhatsapp className="iconsContato"/>
+              <FaWhatsapp className="iconsContato" />
             </a>
             <a href="https://www.facebook.com" className="linksContato">
-              <FaFacebook className="iconsContato"/>
+              <FaFacebook className="iconsContato" />
             </a>
           </div>
+          <div className="divNomesCima">
+            <a>INSTAGRAM</a>
+            <a>(99)99999-9999</a>
+            <a>FACEBOOK</a>
+          </div>
           <div className="divEmail">
-            <HiOutlineMail className="iconsContato"/>
+            <HiOutlineMail className="iconsContato" />
           </div>
           <div className="divLocal">
-            <FaMapMarkerAlt className="iconsContato"/>
+            <FaMapMarkerAlt className="iconsContato" />
           </div>
-
+          <div className="divNomesBaixo">
+            <a>E-mail</a>
+            <a>Rua Alguma Coisa, 100, Liberdade</a>
+            <a>Belo Horizonte - MG</a>
+          </div>
         </div>
       </div>
     </div>

50c3a21ef82103a017cb1780edba19fd5473b032
Hélio Neto <helioneto29@hotmail.com>
2021-01-14 18:11:49 -0300

    fixed tabela quebrada

--- src/Pages/Administrador/OrdersAdm/EspecificOrderAdm/EspecificOrderAdm.js
+++ src/Pages/Administrador/OrdersAdm/EspecificOrderAdm/EspecificOrderAdm.js
@@ -10,88 +10,90 @@ function EspecificOrderAdm() {
 
     return (
         <div className="order-container">
-            <div className="informations">
-                <AiOutlineLeft color='black' size={30} />
-                <div className="title-status">
-                    <span className="title">DETALHES DO PEDIDO</span>
-                    <div className="status">
-                        <span>STATUS: </span>
-                        <div>{status}</div> 
+            <div className="especific-container">
+                <div className="informations">
+                    <AiOutlineLeft color='black' size={30} />
+                    <div className="title-status">
+                        <span className="title">DETALHES DO PEDIDO</span>
+                        <div className="status">
+                            <span>STATUS: </span>
+                            <div>{status}</div> 
+                        </div>
                     </div>
+
+                    <div className="id-button">
+                        <div className="especific-info">
+                            <span className="id">ID: 002</span>
+                            <span className="date">Data do pedido: 01/01/2021</span>
+                            <span className="price">Valor do pedido: R$1000,00</span>
+                        </div>
+                        {
+                            (status === "pending") &&
+                            <div>
+                                <button className="button-status">Mudar status para "Em produção"</button>
+                            </div>
+                        }
+                        {
+                            (status === "preparing") &&
+                            <div>
+                                <input placeholder="código de rastramento" style={{marginRight: "10px"}}></input>
+                                <button className="button-status">Entregar pedido</button>
+                            </div>
+                        }
+                        {
+                            (status === "delivered") &&
+                            <div>
+                                <span className="deliveryman">Entregador: 007</span>
+                            </div>
+                        }
+                    </div>
+
                 </div>
 
-                <div className="id-button">
-                    <div className="especific-info">
-                        <span className="id">ID: 002</span>
-                        <span className="date">Data do pedido: 01/01/2021</span>
-                        <span className="price">Valor do pedido: R$1000,00</span>
-                    </div>
-                    {
-                        (status === "pending") &&
-                        <div>
-                            <button className="button-status">Mudar status para "Em produção"</button>
-                        </div>
-                    }
-                    {
-                        (status === "preparing") &&
-                        <div>
-                            <input placeholder="código de rastramento" style={{marginRight: "10px"}}></input>
-                            <button className="button-status">Entregar pedido</button>
-                        </div>
-                    }
-                    {
-                        (status === "delivered") &&
-                        <div>
-                            <span className="deliveryman">Entregador: 007</span>
-                        </div>
-                    }
-                </div>
-
+                <table className="order-table">
+                    <thead>
+                        <tr className="order-tr-header">
+                            <th className="amount">Quantidade</th>
+                            <th className="products">Produtos</th>
+                            <th className="logo">Logo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="oder-tr-content">
+                            <td className="amount">10</td>
+                            <td className="products">
+                                <img src={camisa} className="image-product" />
+                                <span className="product-name">Camisa Personalisada 1</span>
+                            </td>
+                            <td className="logo">Baixar imagem</td>
+                        </tr>
+                        <tr className="oder-tr-content">
+                            <td className="amount">10</td>
+                            <td className="products">
+                                <img src={camisa} className="image-product" />
+                                <span className="product-name">Camisa Personalisada 1</span>
+                            </td>
+                            <td className="logo">Baixar imagem</td>
+                        </tr>
+                        <tr className="oder-tr-content">
+                            <td className="amount">10</td>
+                            <td className="products">
+                                <img src={camisa} className="image-product" />
+                                <span className="product-name">Camisa Personalisada 1</span>
+                            </td>
+                            <td className="logo">Baixar imagem</td>
+                        </tr>
+                        <tr className="oder-tr-content">
+                            <td className="amount">10</td>
+                            <td className="products">
+                                <img src={camisa} className="image-product" />
+                                <span className="product-name">Camisa Personalisada 1</span>
+                            </td>
+                            <td className="logo">Baixar imagem</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-
-            <table className="order-table">
-                <thead>
-                    <tr className="order-tr-header">
-                        <th className="amount">Quantidade</th>
-                        <th className="products">Produtos</th>
-                        <th className="logo">Logo</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="oder-tr-content">
-                        <td className="amount">10</td>
-                        <td className="products">
-                            <img src={camisa} className="image-product" />
-                            <span className="product-name">Camisa Personalisada 1</span>
-                        </td>
-                        <td className="logo">Baixar imagem</td>
-                    </tr>
-                    <tr className="oder-tr-content">
-                        <td className="amount">10</td>
-                        <td className="products">
-                            <img src={camisa} className="image-product" />
-                            <span className="product-name">Camisa Personalisada 1</span>
-                        </td>
-                        <td className="logo">Baixar imagem</td>
-                    </tr>
-                    <tr className="oder-tr-content">
-                        <td className="amount">10</td>
-                        <td className="products">
-                            <img src={camisa} className="image-product" />
-                            <span className="product-name">Camisa Personalisada 1</span>
-                        </td>
-                        <td className="logo">Baixar imagem</td>
-                    </tr>
-                    <tr className="oder-tr-content">
-                        <td className="amount">10</td>
-                        <td className="products">
-                            <img src={camisa} className="image-product" />
-                            <span className="product-name">Camisa Personalisada 1</span>
-                        </td>
-                        <td className="logo">Baixar imagem</td>
-                    </tr>
-                </tbody>
-            </table>
         </div>
     );
 }

258e9849fb64706ef10115c784c6ab20fa62f272
Raphael Leivas <raphaelleivas@cpejr.com.br>
2021-02-01 03:19:16 -0300

    arrumando bugs do CSS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
     return (
         <LoginContextProvider>
-            <Routes />;
+            <Routes />
         </LoginContextProvider>
     );
 }

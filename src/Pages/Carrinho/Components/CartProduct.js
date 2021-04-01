cd2e8ff07859b2cefd0a862a55ab6c85e62666b3
Diogo Almazan <diogodpa@outlook.com>
2021-04-01 00:34:51 -0300

    Validação para não ter quantidade negativa na tabela da Página Carrinho

--- src/Pages/Carrinho/Components/CartProduct.js
+++ src/Pages/Carrinho/Components/CartProduct.js
@@ -8,6 +8,16 @@ function CartProduct({ handleDelete, index, changeAmount, product }) {
 
   const bucketAWS = process.env.REACT_APP_BUCKET_AWS;
 
+  const handleChangeAmount = (numberToSumOrSub, value, productIndex) => {
+    if(numberToSumOrSub === -1){
+      if(value > 0){
+        changeAmount(numberToSumOrSub, productIndex)
+      }
+    }else{
+      changeAmount(numberToSumOrSub, productIndex)
+    }
+  }
+
   return (
     <tr>
       <td className="productInfo">
@@ -26,7 +36,7 @@ function CartProduct({ handleDelete, index, changeAmount, product }) {
           <IconContext.Provider
             value={{ size: "4vh", className: "pointerCursor" }}
           >
-            <FiMinus onClick={() => changeAmount(-1, index)} />
+            <FiMinus onClick={() => handleChangeAmount(-1, product.amount, index)} />
           </IconContext.Provider>
           <input
             className="amountInput"
@@ -39,7 +49,7 @@ function CartProduct({ handleDelete, index, changeAmount, product }) {
           <IconContext.Provider
             value={{ size: "4vh", className: "pointerCursor" }}
           >
-            <FiPlus onClick={() => changeAmount(1, index)} />
+            <FiPlus onClick={() => handleChangeAmount(1, product.amount, index)} />
           </IconContext.Provider>
         </div>
       </td>

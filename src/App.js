58f82e68d64aa9c7fa8dfb396f773b7929679b2f
Diogo Almazan <diogodpa@outlook.com>
2021-04-28 22:47:44 -0300

    Projeto sem lib bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 
 import Routes from "./routes";
 import LoginContextProvider from "./contexts/LoginContext";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
